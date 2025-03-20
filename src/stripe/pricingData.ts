c59e39b6acb82bffc8355823a1098492076a29f9
Mamadou Coulibaly <ctiefin@gmail.com>
2025-03-20 15:53:11 -0300

    Removed the basic tier plan

--- src/stripe/pricingData.ts
+++ src/stripe/pricingData.ts
@@ -6,16 +6,7 @@ export const pricingData: Price[] = [
     nickname: "Free",
     offers: ["1 AI-generated workout per request", "Basic weather insights"],
   },
-  {
-    id: "price_1Qzfe0FKB8VC7JUerpGTuwKe",
-    unit_amount: 1 * 100,
-    nickname: "Basic",
-    offers: [
-      "2 AI-generated workouts per request",
-      "Daily weather-based fitness tips",
-      "Custom workout preferences",
-    ],
-  },
+
   {
     id: "price_1QzfkQFKB8VC7JUeTvmH6wxv",
     unit_amount: 2 * 100,
