bb1657b7d5d5e40beb5db25c6281bdc7adf80301
YASH-BHAKTA <ybhakta1000@gmail.com>
2025-03-21 11:22:44 -0300

    CI/CD  Updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+require('dotenv').config();
 const nextConfig = {}
 
 module.exports = nextConfig
+
+
+module.exports = {
+  env: {
+    VERCEL_TOKEN: process.env.VERCEL_TOKEN,
+    VERCEL_ORG_ID: process.env.VERCEL_ORG_ID,
+    VERCEL_PROJECT_ID: process.env.VERCEL_PROJECT_ID,
+  },
+};
+
